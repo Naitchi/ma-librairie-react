31e0f991a34afe17c1b85cd142e58063b7c5361f
Naitchi <assasindu39@gmail.com>
2023-08-24 21:04:42 +0200

    🎉 init

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import styles from './index.css';
+/**
+ * Modal component - Represents a reusable modal popup.
+ *
+ * @component
+ * @param {React.Node} content - The content to show in the modal.
+ * @param {function} onClose - The function executed after the modal closes.
+ *
+ * @returns {React.Component} The Modal component.
+ */
+export default function Modal({ content, onClose }) {
+  return (
+    <div onClick={onClose} className={styles.bg}>
+      <div className={styles.modal} id="confirmation">
+        {content}
+        <button className={styles.cross} onClick={onClose}>
+          X
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+  externals: {
+    react: 'commonjs react',
+    'react-dom': 'commonjs react-dom',
+  },
+};
